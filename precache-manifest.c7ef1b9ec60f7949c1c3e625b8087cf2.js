03e51b41c2d8668ef8648bcb93968eb05d4e63c8
IvanChebykin278 <deadkokos4@gmail.com>
2020-04-13 14:15:34 +0500

    second commit

---
+++ precache-manifest.c7ef1b9ec60f7949c1c3e625b8087cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422d63234297255b2a7872abd92e216f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd648a4635bcf2df1b19",
+    "url": "/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "32bd2704dd0372119093",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd648a4635bcf2df1b19",
+    "url": "/static/js/2.829598b7.chunk.js"
+  },
+  {
+    "revision": "e17b5d4e4f5428182a26670adb2e19ff",
+    "url": "/static/js/2.829598b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bd2704dd0372119093",
+    "url": "/static/js/main.783580c1.chunk.js"
+  },
+  {
+    "revision": "7b3fa3a9e7b547134ab5",
+    "url": "/static/js/runtime-main.dd2b30a1.js"
+  }
+]);
